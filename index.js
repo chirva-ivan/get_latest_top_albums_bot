ab9018648c6bac4737c57a50d12b2b6d0113c045
chirva-ivan <ivan.chirva@bia-tech.ru>
2018-10-08 16:48:52 +0300

    add html parsing

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const axios = require("axios");
+const cheerio = require('cheerio');
+const moment = require('moment');
 const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -5,7 +8,33 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'));
+bot.hears('hi', async (ctx) => {
+  try {
+    ctx.reply('loading...');
+
+    const url = 'https://www.albumoftheyear.org/genre/40-metal/2018/';
+    const response = await axios.get(url);
+    const html = cheerio.load(response.data);
+
+    const result = html('.albumListRow')
+      .map((index, domElement) => {
+        const element = html(domElement);
+        const title = element.find('.albumListTitle a').text();
+        const date = element.find('.albumListDate').text();
+        const raiting = element.find('.scoreValue').text();
+
+        return {title, date, raiting}
+      })
+      .get()
+      .sort((a, b) => moment(b.date, 'MMMM D, YYYY') - moment(a.date, 'MMMM D, YYYY'))
+      .map((item) => `${item.title} | ${item.date} | ${item.raiting}/100`);
+
+    return ctx.reply(result.join('\n'));
+  } catch (e) {
+    console.error(e);
+    return ctx.reply('try again later');
+  }
+
+});
 
 bot.startPolling();
