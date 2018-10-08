372c00830cde5ecafc8b8b7c1a7c05aa2e82b1ea
chirva-ivan <ivan.chirva@bia-tech.ru>
2018-10-08 18:46:34 +0300

    minor refactor

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const axios = require("axios");
-const cheerio = require('cheerio');
-const moment = require('moment');
 const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
+const axios = require("axios");
+const cheerio = require('cheerio');
+const moment = require('moment');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const METAL = 'metal';
-const ELECTRONIC = 'electronic';
 const AMBIENT = 'ambient';
+const ELECTRONIC = 'electronic';
+const METAL = 'metal';
 const POST_PUNK = 'post-punk';
 
 bot.start((ctx) => {
@@ -17,8 +17,7 @@ bot.start((ctx) => {
     Markup.keyboard([METAL, ELECTRONIC, AMBIENT, POST_PUNK])
   ))
 });
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.help((ctx) => ctx.reply('Select some music style'));
 
 bot.hears(METAL, getContent('https://www.albumoftheyear.org/genre/40-metal/2018/', METAL));
 bot.hears(ELECTRONIC, getContent('https://www.albumoftheyear.org/genre/6-electronic/2018/', ELECTRONIC));
@@ -29,9 +28,11 @@ function getContent(url, type) {
   return async (ctx) => {
     try {
       ctx.reply(`loading some ${type}...`);
+
       const response = await axios.get(url);
       const html = cheerio.load(response.data);
 
+      // html parsing
       const result = html('.albumListRow')
         .map((index, domElement) => {
           const element = html(domElement);
@@ -39,16 +40,17 @@ function getContent(url, type) {
           const date = element.find('.albumListDate').text();
           const rating = element.find('.scoreValue').text();
 
-          return {title, date, rating}
+          return { title, date, rating }
         })
         .get()
         .sort((a, b) => {
           const dateFormat = 'MMMM D, YYYY';
+          // sorting by release date
           return moment(b.date, dateFormat) - moment(a.date, dateFormat)
         })
-        .map((item) => `${item.title} | ${item.date} | ${item.rating}`);
+        .map((item) => `<code>${item.title}</code>\n<i>${item.date} | Rating: ${item.rating}</i>`);
 
-      return ctx.reply(result.join('\n'));
+      return ctx.replyWithHTML(result.join('\n\n'));
     } catch (e) {
       console.error(e);
       return ctx.reply('try again later');
