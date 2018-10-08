ed70f210d27f6196194045b87becce131da4a8b3
chirva-ivan <ivan.chirva@bia-tech.ru>
2018-10-08 17:18:19 +0300

    add more music

--- index.js
+++ index.js
@@ -8,33 +8,40 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', async (ctx) => {
-  try {
-    ctx.reply('loading...');
 
-    const url = 'https://www.albumoftheyear.org/genre/40-metal/2018/';
-    const response = await axios.get(url);
-    const html = cheerio.load(response.data);
+bot.hears('metal', getContent('https://www.albumoftheyear.org/genre/40-metal/2018/', 'metal'));
+bot.hears('electronic', getContent('https://www.albumoftheyear.org/genre/6-electronic/2018/', 'electronic'));
+bot.hears('ambient', getContent('https://www.albumoftheyear.org/genre/34-ambient/2018/', 'ambient'));
 
-    const result = html('.albumListRow')
-      .map((index, domElement) => {
-        const element = html(domElement);
-        const title = element.find('.albumListTitle a').text();
-        const date = element.find('.albumListDate').text();
-        const raiting = element.find('.scoreValue').text();
+function getContent(url, type) {
+  return async (ctx) => {
+    try {
+      ctx.reply(`loading some ${type}...`);
+      const response = await axios.get(url);
+      const html = cheerio.load(response.data);
 
-        return {title, date, raiting}
-      })
-      .get()
-      .sort((a, b) => moment(b.date, 'MMMM D, YYYY') - moment(a.date, 'MMMM D, YYYY'))
-      .map((item) => `${item.title} | ${item.date} | ${item.raiting}/100`);
+      const result = html('.albumListRow')
+        .map((index, domElement) => {
+          const element = html(domElement);
+          const title = element.find('.albumListTitle a').text();
+          const date = element.find('.albumListDate').text();
+          const rating = element.find('.scoreValue').text();
 
-    return ctx.reply(result.join('\n'));
-  } catch (e) {
-    console.error(e);
-    return ctx.reply('try again later');
+          return {title, date, rating}
+        })
+        .get()
+        .sort((a, b) => {
+          const dateFormat = 'MMMM D, YYYY';
+          return moment(b.date, dateFormat) - moment(a.date, dateFormat)
+        })
+        .map((item) => `${item.title} | ${item.date} | ${item.rating}`);
+
+      return ctx.reply(result.join('\n'));
+    } catch (e) {
+      console.error(e);
+      return ctx.reply('try again later');
+    }
   }
-
-});
+}
 
 bot.startPolling();
