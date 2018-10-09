268379344d4bd0c1cfa57c988ebe445a992b9ad5
chirva-ivan <ivan.chirva@bia-tech.ru>
2018-10-09 11:55:11 +0300

    add micro-bot

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
 const axios = require("axios");
 const cheerio = require('cheerio');
 const moment = require('moment');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
 const AMBIENT = 'ambient';
 const ELECTRONIC = 'electronic';
 const METAL = 'metal';
 const POST_PUNK = 'post-punk';
-
-bot.start((ctx) => {
-  return ctx.replyWithHTML('Select some music', Extra.markup(
-    Markup.keyboard([METAL, ELECTRONIC, AMBIENT, POST_PUNK])
-  ))
-});
-bot.help((ctx) => ctx.reply('Select some music style'));
-
-bot.hears(METAL, getContent('https://www.albumoftheyear.org/genre/40-metal/2018/', METAL));
-bot.hears(ELECTRONIC, getContent('https://www.albumoftheyear.org/genre/6-electronic/2018/', ELECTRONIC));
-bot.hears(AMBIENT, getContent('https://www.albumoftheyear.org/genre/34-ambient/2018/', AMBIENT));
-bot.hears(POST_PUNK, getContent('https://www.albumoftheyear.org/genre/23-post-punk/2018/', POST_PUNK));
 
 function getContent(url, type) {
   return async (ctx) => {
@@ -58,4 +43,26 @@ function getContent(url, type) {
   }
 }
 
-bot.startPolling();
+module.exports = (ctx) => {
+  const message = ctx.update.message.text;
+  const sticker = 'CAADAgADvAIAAuPwEwzKSnJLsIPPdwI';
+
+  switch (message) {
+    case 'start':
+      return ctx.replyWithHTML('Select some music', Extra.markup(
+        Markup.keyboard([METAL, ELECTRONIC, AMBIENT, POST_PUNK])
+      ));
+    case 'help':
+      return ctx.reply('Select some music style');
+    case METAL:
+      return  getContent('https://www.albumoftheyear.org/genre/40-metal/2018/', METAL)(ctx);
+    case ELECTRONIC:
+      return  getContent('https://www.albumoftheyear.org/genre/6-electronic/2018/', ELECTRONIC)(ctx);
+    case AMBIENT:
+      return  getContent('https://www.albumoftheyear.org/genre/34-ambient/2018/', AMBIENT)(ctx);
+    case POST_PUNK:
+      return getContent('https://www.albumoftheyear.org/genre/23-post-punk/2018/', POST_PUNK)(ctx);
+    default:
+      return ctx.replyWithSticker(sticker);
+  }
+};
